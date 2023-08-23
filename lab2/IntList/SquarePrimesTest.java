73f4b6cde5a1573573b1b7588dce3bbcd7c5a14a
Zhihan Tuo <johntuozh@gmail.com>
2023-08-22 19:01:08 -0700

    Added 3 additional tests to verify for correct behavior from squarePrimes method

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,28 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesAllPrimes() {
+        IntList lst = IntList.of(2, 3, 5, 7, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesNoPrimes() {
+        IntList lst = IntList.of(4, 6, 8, 9, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 8 -> 9 -> 10", lst.toString());
+        assertFalse(changed);
+    }
+
+   @Test
+   public void testSquarePrimeMixed() {
+        IntList lst = IntList.of(17, 22, 23, 25, 27);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("289 -> 22 -> 529 -> 25 -> 27", lst.toString());
+        assertTrue(changed);
+   }
 }
