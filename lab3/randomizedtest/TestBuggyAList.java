87d1ca48df9f931bc0eaaf42ec8d8b1d4609dc47
Zhihan Tuo <johntuozh@gmail.com>
2024-04-20 15:41:26 -0700

    Added randomizedTest

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -21,4 +21,24 @@ public class TestBuggyAList {
         assertEquals(NoResize.removeLast(), Buggy.removeLast());
         assertEquals(NoResize.removeLast(), Buggy.removeLast());
     }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            }
+        }
+    }
 }
