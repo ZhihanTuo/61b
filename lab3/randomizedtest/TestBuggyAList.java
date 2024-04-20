c1e521f0e71c1a1a0f40141cbc29f8b187cae746
Zhihan Tuo <johntuozh@gmail.com>
2024-04-20 15:30:23 -0700

    Added new test ThreeAddThreeRemove

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,17 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> NoResize = new AListNoResizing<>();
+        BuggyAList<Integer> Buggy = new BuggyAList<>();
+        for (int i : new int[]{7, 8, 9}) {
+            NoResize.addLast(i);
+            Buggy.addLast(i);
+        }
+
+        assertEquals(NoResize.removeLast(), Buggy.removeLast());
+        assertEquals(NoResize.removeLast(), Buggy.removeLast());
+        assertEquals(NoResize.removeLast(), Buggy.removeLast());
+    }
 }
