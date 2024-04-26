97444773983e94c5db6928966d100d8f5811b030
Zhihan Tuo <johntuozh@gmail.com>
2024-04-25 20:45:36 -0700

    Implemented default isEmpty method in the interface, removed the isEmpty methods implemented in ArrayDeque and LinkedListDeque

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -5,12 +5,12 @@ import java.util.Iterator;
 public interface Deque<T> {
     public void addFirst(T Item);
     public void addLast(T Item);
-    public boolean isEmpty();
+    public default boolean isEmpty() {
+        return size() == 0;
+    }
     public int size();
     public void printDeque();
     public T removeFirst();
     public T removeLast();
     public T get(int index);
-    public Iterator<T> iterator();
-    public boolean equals(Object o);
 }
