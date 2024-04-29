2f3c6ec898f7701f8915551b3bd5ac27fe881b50
Zhihan Tuo <johntuozh@gmail.com>
2024-04-28 23:38:52 -0700

    Added new MaxArrayDeque class, which inherits from ArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,2 +1,27 @@
-package deque;public class MaxArrayDeque {
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    private Comparator<T> comparator;
+
+    /** Creates a MaxArrayDeque with given Comparator */
+    public MaxArrayDeque(Comparator<T> c) {
+        comparator = c;
+    }
+
+    /** Returns the max element in the deque as governed by previous Comparator
+     * Returns null if MaxArrayDeque is empty */
+    public T max() {
+        return null;
+    }
+
+    /** Returns the max element in the deque as governed by the Comparator c
+     * Returns null if MaxDequeArray is empty */
+    public T max(Comparator<T> c) {
+        comparator = c;
+        return null;
+    }
+
+
 }
